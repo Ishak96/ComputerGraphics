33e7efac97bbe3cc33fe6005c6b3a4fe6ce10770
Ishak96 <ishakayad96@outlook.fr>
2024-09-01 18:38:41 +0200

    .gitignore

--- introduction/.history/assests/vertex_shader_20240901170455.glsl
+++
@@ -1,9 +0,0 @@
-// Vertex Shader Source Code (GLSL)
-#version 330 core
-layout (location = 0) in vec3 aPos;
-
-void main()
-{
-    // Normalize the coordinates based on the window size
-    gl_Position = vec4(aPos, 1.0);
-}
--- introduction/.history/src/triangle_20240901161327.cpp
+++
@@ -1,9 +0,0 @@
-#include "triangle.h"
-#include <iostream>
-
-Triangle::Triangle(float* vertices, glm::vec4 color)
-    : vertices(vertices), color(color), VBO(0), VAO(0)
-{
-
-}
-
